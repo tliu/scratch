3c8c6849ce23af408cfa7676a3e26ebabeb57e99
Thomas Liu <thomas.liu@vivint.com>
2019-07-19 14:58:17 -0400

    tests

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module source.vivint.com/pl/scratch
 
 go 1.12
 
-require source.vivint.com/pl/mongo v1.1.0-20190620.0.20190711223009-7675d8b3c906
+require (
+	go.mongodb.org/mongo-driver v1.0.4
+	source.vivint.com/pl/accountsystem v1.1.0
+	source.vivint.com/pl/log v1.0.0-20190523
+	source.vivint.com/pl/mongo v1.1.0-20190620.0.20190719185458-877340ec9d61
+)
--- main.go
+++ main.go
@@ -1,16 +1,53 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	//"go.mongodb.org/mongo-driver/bson/primitive"
+	"source.vivint.com/pl/log"
 	"source.vivint.com/pl/mongo"
-	"source.vivint.com/pl/mongo/options"
+	"time"
 )
 
-func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(*mongoT    imeout)*time.Minute)
-	defer cancel()
-	c := mongo.NewClient().SetAuth("skyPlatform", "mango").SetHost("mongodb://localhost:27017")
-	err := c.Connect(ctx)
+type test struct {
+	Name int    `bson:"a"`
+	Data string `bson:"b"`
+}
 
+func main() {
+	log.Debug("test", log.Fields{})
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(20)*time.Minute)
+	defer cancel()
+	c := mongo.NewClient().SetAuth("skyPlatform", "mango").SetHost("localhost:27017")
+	c = c.SetRetrier(5, .05, 10)
+
+	err := c.Connect(ctx)
+	if err != nil {
+		fmt.Println(err)
+	}
+	res := []test{}
+	opts := mongo.FindOptions{}
+	x := mongo.FieldsToProjection([]string{"b"})
+	opts.SetProjection(x)
+	err = c.Database("thor").Collection("test").Find(mongo.QueryMap{}, &res, &opts)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(res)
+	var i int64
+	i, err = c.Database("thor").Collection("test").Count(bson.M{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(i)
+	/*
+		var q interface{}
+		q, err = c.Database("thor").Collection("test").InsertOne(bson.M{"a": 10, "b": "foo!!"})
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(q)
+	*/
 
 }
